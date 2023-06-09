62605fcce0b6f0da4be5a02773d969dca80595d8
mr-exception <pitronum@gmail.com>
2023-06-09 17:06:44 +0300

    completed main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Layout, Menu } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
+import { Route, Routes, useMatch, useNavigate } from "react-router-dom";
+import Home from "./Containers/Home/Home";
+import Transactions from "./Containers/Transactions/Transactions";
+import Users from "./Containers/Users/Users";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const navigate = useNavigate();
 
+  const defaultKey = [];
+
+  const isHome = useMatch("/");
+  const isUsers = useMatch("/users");
+  const isTransactions = useMatch("/transactions");
+  if (isTransactions) {
+    defaultKey.push("transactions");
+  } else if (isUsers) {
+    defaultKey.push("users");
+  } else if (isHome) {
+    defaultKey.push("home");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Layout className="layout">
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={defaultKey}
+          items={[
+            {
+              key: "home",
+              label: "Home",
+              onClick: () => {
+                navigate("/");
+              },
+            },
+            {
+              key: "users",
+              label: "Users",
+              onClick: () => {
+                navigate("/users");
+              },
+            },
+            {
+              key: "transactions",
+              label: "Transactions",
+              onClick: () => {
+                navigate("/transactions");
+              },
+            },
+          ]}
+        />
+      </Header>
+      <Content style={{ minHeight: "calc(100vh - 64px)" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/transactions" element={<Transactions />} />
+        </Routes>
+      </Content>
+    </Layout>
+  );
 }
-
-export default App
