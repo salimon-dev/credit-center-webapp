1d3d637775aee11fb7ca87f2a2a1b9056f693e20
mr-exception <pitronum@gmail.com>
2023-06-13 12:48:05 +0300

    added github workflow

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AuthContextProvider } from "./Providers/AuthProvider.tsx";
-import { DialogsContextProvider } from "./Providers/DialogsProvider.tsx";
 
 const queryClient = new QueryClient();
 
@@ -14,9 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <AuthContextProvider>
-          <DialogsContextProvider>
-            <App />
-          </DialogsContextProvider>
+          <App />
         </AuthContextProvider>
       </BrowserRouter>
     </QueryClientProvider>
